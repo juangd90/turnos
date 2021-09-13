33d8364b7ddc8acf405852f0906557938df0aac5
juangd90 <juan.gandulfo90@gmail.com>
2021-09-13 17:14:23 -0300

    Borra horarios

--- app/models.py
+++ app/models.py
@@ -17,14 +17,7 @@ horas=(
 
 )
 
-class Dia(models.Model):
-    dia=models.DateField()
-    disponible=models.BooleanField(default=True)
 
-    
-
-    def __str__(self):
-        return f"{self.dia}"
 
 
 class Hora(models.Model):
@@ -33,17 +26,29 @@ class Hora(models.Model):
 
     def __str__(self):
         return f"{self.horas}"
+        
+class Dia(models.Model):
+    dia=models.DateField()
+    disponible=models.BooleanField(default=True)
+    horario=models.ManyToManyField(Hora)
+    
+    #hacer metodo para borrar el horario
+    def BorraHorario(self,hora):
+        horarios=self.horario.all() #traigo todos los horarios disponibles
+        for h in horarios: #recorro esos horarios
+            if h.horas==hora:# si el horario coincide con lo que recibe por parametro
+                self.horario.remove(h)#lo borra de la lista de horarios de ese dia
+        self.save()#guarda los cambios   
+            
 
+    def __str__(self):
+        return f"{self.dia}"
 
 class Turno(models.Model):
     nombre=models.CharField(max_length=64)
+    dni=models.IntegerField(max_length=8,default=0)
     dia=models.ForeignKey(Dia,on_delete=models.CASCADE)
-    hora=models.ForeignKey(Hora,on_delete=models.CASCADE)
-
-    def CambiaDisponible(self,dia,hora):
-        self.dia.disponible=False
-        self.hora.disponible=False
-        self.save()
+    hora=models.ForeignKey(Hora,on_delete=models.CASCADE)   
 
     def __str__(self):
         return f"Turno el día {self.dia} a las {self.hora} para {self.nombre}"
--- app/views.py
+++ app/views.py
@@ -12,20 +12,32 @@ def index(request):
     form=Turno
     fields="__all__"
     success_url=reverse_lazy('index') '''
-#hay que traer los dias y horarios disponibles, y en base a eso mostrar el calendario
-#ademas tiene que recibir la fecha y hora elegida y con eso cambiar el estado disponible a false
-# con el get_or_create
+
 def Turnos(request):
     if request.method=='POST':
-        ...
+        dia=request.POST.get('fecha')#trae la fecha seleccionada
+        horas=request.POST.get('horas')
+        nombre=request.POST.get('nombre')
+        prueba,created=Dia.objects.get_or_create(dia=dia)
+        hora,created=Hora.objects.get_or_create(horas=horas)
+        #prueba.BorraHorario("11:00")
+        turno=Turno(nombre=nombre,dni=12345678,dia=prueba,hora=hora)
+        turno.save()
+        return render(request,"app/index.html",{
+            'turno':turno
+        })
     else:
-        primer_dia=Dia.objects.filter(disponible=True).first()
-        ultimo_dia=Dia.objects.filter(disponible=True).last()
+        primer_dia=Dia.objects.first()
+        ultimo_dia=Dia.objects.last()
         horas=Hora.objects.filter(disponible=True)
+        turno=Turno.objects.all()
+        
         return render(request,"app/turnos.html",{
             'primer_dia':primer_dia,
             'ultimo_dia':ultimo_dia,
             'horas':horas,
+            'turno':turno,
+            
         })
 
 
